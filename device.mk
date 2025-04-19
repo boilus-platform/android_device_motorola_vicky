2288b9eb1433bd7f6987bef7cc72f355b2f558de
boiler <155823492+boilus-platform@users.noreply.github.com>
2025-04-19 19:15:30 +0200

    Update device.mk

--- device.mk
+++ device.mk
@@ -16,16 +16,12 @@ AB_OTA_POSTINSTALL_CONFIG += \
 # Boot control HAL
 PRODUCT_PACKAGES += \
     android.hardware.boot@1.0-impl \
+    bootctrl.mt6789 \
+    bootctrl.mt6789.recovery \
     android.hardware.boot@1.0-service
 
 PRODUCT_PACKAGES += \
-    bootctrl.mt6789
-
-PRODUCT_STATIC_BOOT_CONTROL_HAL := \
-    bootctrl.mt6789 \
-    libgptutils \
-    libz \
-    libcutils
+    bootctrl
 
 PRODUCT_PACKAGES += \
     otapreopt_script \
